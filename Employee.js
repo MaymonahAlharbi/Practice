44005d229b31f0fbb9e7def02814be899291a883
Maymonah <maymonah.10@hotmail.com>
2024-09-27 00:56:28 +0300

    Some functions added

--- Employee.js
+++ Employee.js
@@ -1,8 +1,15 @@
 function displayName(name) {
     console.log("Welecom to DevOps World, " + name + "!");
 }
-displayName("Maymonah");
 
 function displayJobTitle(jobTitle) {
     console.log("Job Title: "+jobTitle)
 }
+
+function displayJoinDate(JoinDate) {
+    console.log("Employee Join Date: "+JoinDate)
+}
+
+displayName("Maymonah");
+displayJobTitle("Software Engineer");
+displayJoinDate("01-01-2024");
