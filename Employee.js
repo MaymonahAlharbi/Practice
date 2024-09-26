d8c705aa81418f3455be3787095f976f8b227fdf
Maymonah <maymonah.10@hotmail.com>
2024-09-26 23:38:35 +0300

    Job title function added

--- Employee.js
+++ Employee.js
@@ -2,3 +2,7 @@ function displayName(name) {
     console.log("Welecom to DevOps World, " + name + "!");
 }
 displayName("Maymonah");
+
+function displayJobTitle(jobTitle) {
+    console.log("Job Title: "+jobTitle)
+}
